12e2ceb76e02fd3adf86f7808dd04e3fca76e028
Pratham <130177487+Pratham-99-Sh@users.noreply.github.com>
2023-07-30 12:40:59 +0000

    shopping Cart backend

--- cartMain.cpp
+++ cartMain.cpp
@@ -1,84 +1,86 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include "dataModel.h"
+
 using namespace std;
 
-class Cart {
-private:
-    int* cart;
-    int size;
-    int capacity;
-public:
-
-    Cart() {
-        size = 0;
-        capacity = 10;
-        cart = new int[capacity];
-    }
-
-    void add(int item) {
-        if (size == capacity) {
-            int* temp = new int[capacity * 2];
-            for (int i = 0; i < size; i++) {
-                temp[i] = cart[i];
-            }
-            delete[] cart;
-            cart = temp;
-            capacity *= 2;
-        }
-        cart[size] = item;
-        size++;
-    }
-
-    void remove(int item) {
-        int index = -1;
-        for (int i = 0; i < size; i++) {
-            if (cart[i] == item) {
-                index = i;
-                break;
-            }
-        }
-        if (index == -1) {
-            cout << "Item not found" << endl;
-            return;
-        }
-        for (int i = index; i < size - 1; i++) {
-            cart[i] = cart[i + 1];
-        }
-        size--;
-    }
-
-    void print() {
-        for (int i = 0; i < size; i++) {
-            cout << cart[i] << " ";
-        }
-        cout << endl;
-    }
-
-    ~Cart() {
-        delete[] cart;
-    }
+vector<Product> allProducts = {
+    Product(1, "Apple", 100),
+    Product(2, "Banana", 50),
+    Product(3, "Orange", 80),
+    Product(4, "Mango", 150),
+    Product(5, "Pineapple", 200),
+    Product(6, "Grapes", 120),
+    Product(7, "Watermelon", 80),
+    Product(8, "Kiwi", 100),
+    Product(9, "Papaya", 70),
+    Product(10, "Guava", 60)
 };
 
-int main() {
-    Cart c;
-    c.add(1);
-    c.add(2);
-    c.add(3);
-    c.add(4);
-    c.add(5);
-    c.add(6);
-    c.add(7);
-    c.add(8);
-    c.add(9);
-    c.add(10);
-    c.add(11);
-    c.print();
-    c.remove(5);
-    c.print();
-    c.remove(11);
-    c.print();
-    c.remove(1);
-    c.print();
-    c.remove(11);
-    c.print();
+Product* chooseProduct(){
+    string productList;
+    for(auto pd : allProducts)
+        productList.append(pd.getDisplayName());
+
+    cout << productList << endl;
+    cout << "......................" << endl;
+    string choice;
+    cin >> choice;
+
+    for(auto pd : allProducts)
+        if(pd.getShortName() == choice)
+            return &pd;
+
+    cout << "Product not found!" << endl;
+    return NULL;
+}
+
+bool checkout(Cart &cart){
+    if(cart.isEmpty()){
+        cout << "Cart is empty!" << endl;
+        return false;
+    }
+
+    int total = cart.getTotalPrice();
+    cout << "Total: Rs." << total << endl;
+    cout << "Pay in Cash : Rs. ";
+    int paid; cin >> paid;
+    
+    if(paid < total){
+        cout << "Insufficient amount!" << endl;
+        return false;
+    }
+    else{
+        int change = paid - total;
+        if(change) cout<< "Change: Rs." << change << endl;
+        cout << "Thank you for shopping with us!" << endl;
+        return true;
+    }
+}
+
+int main(){
+    char action;
+    Cart cart;
+    while(true){
+       cout << "Choose action: (a)dd item,  (v)iew cart,  (c)heckout" << endl;
+       cin >> action;
+       if(action == 'a'){
+            Product* pd = chooseProduct();
+            if(pd!=NULL){
+                cout << "Product added to cart : " + pd->getDisplayName() << endl;
+                cart.addProduct(*pd);
+            }
+       }
+       else if(action == 'v'){
+        cout << "......................" << endl;
+        cart.viewCart();
+        cout << "......................" << endl;
+       }
+       else{
+            cart.viewCart();
+            if(checkout(cart))
+                break;
+       }
+    }
     return 0;
 }
